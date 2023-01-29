bec6a261809aa5f720108fc06e57412d8efbb5c6
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2023-01-28 23:36:20 -0300

    Primeiro commit (criado o projeto)

---
+++ src/test/java/br/dev/paulowolfgang/serviceorderapi/ServiceOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.paulowolfgang.serviceorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
