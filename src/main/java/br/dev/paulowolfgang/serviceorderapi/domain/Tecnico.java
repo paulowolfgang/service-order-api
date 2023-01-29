e8cf4c55a78639d32700c82b3ba94cee90bdf04c
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2023-01-28 23:58:01 -0300

    Criadas classes de domínio

---
+++ src/main/java/br/dev/paulowolfgang/serviceorderapi/domain/Tecnico.java
@@ -0,0 +1,13 @@
+package br.dev.paulowolfgang.serviceorderapi.domain;
+
+public class Tecnico extends Pessoa {
+
+	public Tecnico() {
+		super();
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+	}
+	
+}
